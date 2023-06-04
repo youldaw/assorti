2647611ff9f74f28960512187c4abb5a4d7081a1
youldaw <68648932+youldaw@users.noreply.github.com>
2023-06-05 04:39:26 +0500

    header
    footer

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    $('input[name=phone]').mask('+7 (999) 999-99-99');
+    // $('input[name=phone]').mask('+7 (999) 999-99-99');
 
 
     $('.menu-opener').on('click', function (e) {
@@ -9,31 +9,32 @@ $(function () {
         $('.navbar').toggleClass('active');
     });
 
-    var swiper3 = new Swiper(".quality-slide-in", {
-        slidesPerView: 1,
-        spaceBetween: 10,
-        loop: true,
-        speed: 1000,
-        pagination: {
-            el: ".swiper-pagination",
-            clickable: true,
-        },
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        breakpoints: {
-            640: {
-                slidesPerView: 1,
-            },
-            768: {
-                slidesPerView: 1,
-            },
-            1024: {
-                slidesPerView: 1,
-            },
-        },
-    });
+    // var swiper3 = new Swiper(".quality-slide-in", {
+    //     slidesPerView: 1,
+    //     spaceBetween: 10,
+    //     loop: true,
+    //     speed: 1000,
+    //     pagination: {
+    //         el: ".swiper-pagination",
+    //         clickable: true,
+    //     },
+    //     navigation: {
+    //         nextEl: ".swiper-button-next",
+    //         prevEl: ".swiper-button-prev",
+    //     },
+    //     breakpoints: {
+    //         640: {
+    //             slidesPerView: 1,
+    //         },
+    //         768: {
+    //             slidesPerView: 1,
+    //         },
+    //         1024: {
+    //             slidesPerView: 1,
+    //         },
+    //     },
+    // });
+    
 
 });
 
