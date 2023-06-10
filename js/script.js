225167c4f403c993d6f6dddfa4b04df54e725b15
youldaw <68648932+youldaw@users.noreply.github.com>
2023-06-10 22:59:51 +0500

    meniki
    meniki

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(function () {
 
-    // $('input[name=phone]').mask('+7 (999) 999-99-99');
+    $('input[name=phone]').mask('+7 (999) 999-99-99');
 
 
     $('.menu-opener').on('click', function (e) {
@@ -34,7 +34,76 @@ $(function () {
     //         },
     //     },
     // });
+
+
+    // input type file
+    $("#FileInput").on('change', function (e) {
+        var labelVal = $(".file-title").text();
+        var oldfileName = $(this).val();
+        fileName = e.target.value.split('\\').pop();
+
+        if (oldfileName == fileName) { return false; }
+        var extension = fileName.split('.').pop();
+
+        if ($.inArray(extension, ['jpg', 'jpeg', 'png']) >= 0) {
+            $(".filelabel i, .filelabel .file-title").css({ 'color': '#208440' });
+            $(".filelabel").css({ 'border': ' 2px solid #208440' });
+        }
+        else if (extension == 'pdf') {
+            $(".filelabel").css({ 'border': ' 2px solid red' });
+
+        }
+        else if (extension == 'doc' || extension == 'docx') {
+            $(".filelabel").css({ 'border': ' 2px solid #2388df' });
+        }
+        else {
+            $(".filelabel i, .filelabel .file-title").css({ 'color': 'black' });
+            $(".filelabel").css({ 'border': ' 2px solid black' });
+        }
+
+        if (fileName) {
+            if (fileName.length > 10) {
+                $(".filelabel .file-title").text(fileName.slice(0, 4) + '...' + extension);
+            }
+            else {
+                $(".filelabel .file-title").text(fileName);
+            }
+        }
+        else {
+            $(".filelabel .file-title").text(labelVal);
+        }
+    });
     
+    // custom select
+    const selects = document.querySelectorAll('.select-menu');
+
+	selects.forEach(item => {
+		let select = item.querySelector('.select');
+		let options = item.querySelectorAll('.option');
+		let optionList = item.querySelector('.options-list');
+		select.onclick = function () {
+			this.classList.toggle('active');
+			this.classList.toggle('select_active')
+			optionList.classList.toggle('active');
+		}
+
+		options.forEach(option => {
+			option.onclick = function () {
+				options.forEach(a => {
+					a.classList.remove('selected');
+				})
+				this.classList.add('selected');
+				let parent = this.parentNode.parentNode;
+				let select = parent.querySelector('.select');
+				let options_list = parent.querySelector('.options-list');
+				select.classList.remove('active');
+				select.classList.remove('select_active');
+				options_list.classList.remove('active');
+				select.querySelector('span').innerHTML = this.innerHTML;
+			}
+		})
+	});
+
 
 });
 
