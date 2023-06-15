fb2dd55d56d4824af6c2da379d27fdfd126f7f7f
youldaw <68648932+youldaw@users.noreply.github.com>
2023-06-15 06:31:22 +0500

    oxiri
    oxiri

--- js/script.js
+++ js/script.js
@@ -35,6 +35,64 @@ $(function () {
         },
     });
 
+    var swiper3 = new Swiper(".company-slide", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: true,
+        speed: 1000,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 1,
+            },
+        },
+    });
+
+    // main-slide
+    var mainSwiper = new Swiper(".main-slide", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+        loop: false,
+        speed: 1000,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
+    var pagingSwiper = new Swiper(".main-slide", {
+        pagination: {
+            el: ".swiper-pagination2",
+            type: "fraction",
+            formatFractionCurrent: function (number) {
+                return ('0' + number).slice(-2);
+            },
+            formatFractionTotal: function (number) {
+                return ('0' + number).slice(-2);
+            },
+            renderFraction: function (current, total) {
+                return '<span class="' + current + '"></span>' +
+                       '<p>  </p>' +
+                       '<span class="' + total + '"></span>';
+            }
+        },
+    });
+
+    mainSwiper.controller.control = pagingSwiper
+
 
     // input type file
     $("#FileInput").on('change', function (e) {
@@ -73,60 +131,60 @@ $(function () {
             $(".filelabel .file-title").text(labelVal);
         }
     });
-    
+
     // custom select
     const selects = document.querySelectorAll('.select-menu');
 
-	selects.forEach(item => {
-		let select = item.querySelector('.select');
-		let options = item.querySelectorAll('.option');
-		let optionList = item.querySelector('.options-list');
-		select.onclick = function () {
-			this.classList.toggle('active');
-			this.classList.toggle('select_active')
-			optionList.classList.toggle('active');
-		}
+    selects.forEach(item => {
+        let select = item.querySelector('.select');
+        let options = item.querySelectorAll('.option');
+        let optionList = item.querySelector('.options-list');
+        select.onclick = function () {
+            this.classList.toggle('active');
+            this.classList.toggle('select_active')
+            optionList.classList.toggle('active');
+        }
 
-		options.forEach(option => {
-			option.onclick = function () {
-				options.forEach(a => {
-					a.classList.remove('selected');
-				})
-				this.classList.add('selected');
-				let parent = this.parentNode.parentNode;
-				let select = parent.querySelector('.select');
-				let options_list = parent.querySelector('.options-list');
-				select.classList.remove('active');
-				select.classList.remove('select_active');
-				options_list.classList.remove('active');
-				select.querySelector('span').innerHTML = this.innerHTML;
-			}
-		})
-	});
+        options.forEach(option => {
+            option.onclick = function () {
+                options.forEach(a => {
+                    a.classList.remove('selected');
+                })
+                this.classList.add('selected');
+                let parent = this.parentNode.parentNode;
+                let select = parent.querySelector('.select');
+                let options_list = parent.querySelector('.options-list');
+                select.classList.remove('active');
+                select.classList.remove('select_active');
+                options_list.classList.remove('active');
+                select.querySelector('span').innerHTML = this.innerHTML;
+            }
+        })
+    });
 
 
-	var swiper = new Swiper('.mySwiper', {
-		speed: 1000,
-		slidesPerView: 7,
-		spaceBetween: 70,
-		loop: true,
-		navigation: {
-			nextEl: '.swiper-button-next',
-			prevEl: '.swiper-button-prev',
-		},
-	});
+    var swiper = new Swiper('.mySwiper', {
+        speed: 1000,
+        slidesPerView: 7,
+        spaceBetween: 70,
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
 
-	try {
-		const swiper = new Swiper('.mySwiper3', {
-			loop: true,
-			slidesPerView: 1,
-			pagination: {
-				el: '.swiper-pagination',
-			},
-		});
-	} catch (error) {
-		
-	}
+    try {
+        const swiper = new Swiper('.mySwiper3', {
+            loop: true,
+            slidesPerView: 1,
+            pagination: {
+                el: '.swiper-pagination',
+            },
+        });
+    } catch (error) {
+
+    }
 
 
 });
